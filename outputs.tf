c2cfbf3beab702613235f1c95198d6e12f771064
manikandan <rajavetrivelmani@gmail.com>
2024-07-15 17:18:44 +0530

    re

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-# output "testing_ec2_alb_sg_id" {
-#   description = "The ID of the testing EC2 ALB security group"
-#   value       = aws_security_group.testing_ec2_alb_sg.id
-# }
-
-# output "testing_ec2_sg_id" {
-#   description = "The ID of the testing EC2 security group"
-#   value       = aws_security_group.testing_ec2_sg.id
-# }
